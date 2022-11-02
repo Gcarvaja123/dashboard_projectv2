8fbd664a23799d6e15ad7fe6f207c9c3583bb4c8
Gabriel Carvajal Marambio <gabriel.carvajal.14@sansano.usm.cl>
2022-11-01 23:42:07 -0300

    agregar version

--- Models/index.js
+++ Models/index.js
@@ -11,9 +11,11 @@ var opts = {
     }
 }
 const Op = Sequelize.Op;
-var sequelize = new Sequelize('trabajo','admin','Xvkgmdwe135', {
+var sequelize = new Sequelize('trabajo','admin','xvkgmdwe135', {
   host: 'dashboarddb.cbqwyeck8ajg.us-east-2.rds.amazonaws.com',
   dialect: 'mysql',
+  username : 'admin',
+  password : "xvkgmdwe135"
   port : 3306,
   operatorsAliases: Op, // use Sequelize.Op
   pool: {
